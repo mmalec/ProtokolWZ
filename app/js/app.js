41c96d52aa7dc667f4a9abc7ad3310a21e2a7013
m.malec <m.malec@WSKR1.kwpsp.psp.wlkp.pl>
2013-12-29 07:16:08 +0100

    bootstrap

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,52 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+var app = angular.module('myApp', [
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers',
+    'ui.bootstrap'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+        config(['$routeProvider', function($routeProvider) {
+                $routeProvider
+                        .when('/view1/', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'})
+                        .when('/view2/', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'})
+                        .otherwise({redirectTo: '/view1/'});
+            }]);
+
+
+app.run( function($rootScope) {
+    var protokul = {
+    nazwa:"Moja nazwa",
+    objekt:{
+            id:"1"
+    }
+}
+
+    var db = new PouchDB('protocols');
+    db.put({
+        _id: 'mydoc',
+        title: 'Heroes'
+    }, function(err, response) {
+    });
+
+    db.put({
+        _id: 'mydoc',
+        _rev: '1-A6157A5EA545C99B00FF904EEF05FD9F',
+        title: 'Lets Dance',
+    }, function(err, response) {
+    })
+
+    db.info(function(err, info) {
+        console.log(info.db_name);
+    });
+    $rootScope.db=db
+    
+    $rootScope.protokul = protokul;
+    
+
+
+})
+
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,53 @@
-'use strict';
+//'use strict';
 
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+        controller('MyCtrl1', function($rootScope, $scope, $q, $timeout) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+            var name
+            var i = 1
+            $scope.laduj = function() {
 
-  }]);
+                //  alert("dziala klick")
+                // $scope.name = name;
+                var defer = $q.defer();
+
+                alert("timeout")
+                //  defer.resolve(function() {
+                alert("resolve")
+                i = i + 1
+                $scope.db.info(function(err, info) {
+                    alert("start info")
+                    i = i + 1
+                    $scope.name = "drugie"
+                    $scope.text = "To jest text"
+                    $scope.$apply();
+                    alert("i " + i)
+                    defer.resolve(function(){
+                        alert("w resolve")
+                    });
+                })
+                alert("promise " + defer.promise)
+                var protokul = $rootScope.protokul;
+                
+                //$scope.i = defer.promise
+                //   });
+
+                // $timeout(function() {
+
+                // }, 200);
+
+//return defer.promise 
+
+
+            }
+            $scope.name = "trzecie"
+            $scope.$watch('pierwszy', function (){
+                //alert("zmiana "+ $scope.pierwszy)
+            })
+            //alert(name)
+        })
+        .controller('MyCtrl2', [function() {
+
+            }]);
